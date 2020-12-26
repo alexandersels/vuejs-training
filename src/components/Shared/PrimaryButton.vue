15659c6c2c68d275f9f5ed5a498aa4c5589e8a6e
Alexander Sels <alexandersels@hotmail.com>
2020-12-26 16:40:18 +0100

    .

---
+++ src/components/Shared/PrimaryButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button @click="clicked"> {{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+  props: ['text', 'clicked'],
+}
+</script>
+
+<style scoped>
+
+
+button {
+  cursor: pointer;
+  display: inline-block;
+  padding: 1rem 5rem;
+  margin: 1rem 0;
+
+  font-size: 1.8rem;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+
+  border-radius: 50rem;
+  border: 3px solid #007bff;
+  color: #007bff;
+  background-color: transparent;
+  outline: none;
+  text-decoration: none;
+
+  transition: all 0.5s ease;
+}
+
+button:hover,
+button:focus,
+button:active {
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+}
+
+button:disabled {
+  cursor: default;
+  box-shadow: none !important;
+  color: #ced4da !important;
+  border-color: #ced4da !important;
+  background-color: transparent !important;
+}
+</style>
--- src/components/Users/Create-User.vue
+++ src/components/Users/Create-User.vue
@@ -1,15 +1,51 @@
 <template>
   <div class="container">
     <PageTitle title="Register a new user"></PageTitle>
+    <form class="form">
+      <div class="row">
+        <input v-model=formData.firstname name="firstname" class="form__field" required placeholder="Firstname">
+      </div>
+      <div class="row">
+        <input v-model=formData.lastname name="lastname" class="form__field" required placeholder="Lastname">
+      </div>
+      <div class="row">
+        <input v-model=formData.email name="email" class="form__field" required placeholder="Email">
+      </div>
+      <div class="row">
+        <input v-model=formData.password name="password" type="password" class="form__field" required
+               placeholder="Password">
+      </div>
+    </form>
+    <div class="row">
+      <PrimaryButton text="Register" :clicked=onRegisterClicked></PrimaryButton>
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from "@/components/Shared/PageTitle";
+import PrimaryButton from "@/components/Shared/PrimaryButton";
 
 export default {
   name: "Create-User",
-  components: {PageTitle}
+  components: {PrimaryButton, PageTitle},
+  data() {
+    return {
+      formData: {
+        firstname: undefined,
+        lastname: undefined,
+        email: undefined,
+        password: undefined,
+      }
+    }
+  },
+  methods: {
+    onRegisterClicked() {
+      const user = JSON.parse(JSON.stringify(this.formData));
+      console.log(user);
+      // axios post
+    }
+  }
 }
 </script>
 
@@ -17,4 +53,42 @@ export default {
 .container {
   margin-top: 10rem;
 }
+
+.form .row {
+  margin-top: 2rem;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.form__field {
+  background: #fff;
+  color: #4c4d4d;
+  font-size: 1.6rem;
+
+  display: block;
+  width: 100%;
+
+  padding: 1rem;
+
+  border: 1px solid #ced4da;
+  border-radius: .25rem;
+
+  overflow: visible;
+  outline: none;
+  resize: vertical;
+
+  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+}
+
+.form__field:focus,
+.form__field:active {
+  border-color: #007bff !important;
+  box-shadow: 0 0 5px #007bff;
+}
+
+
 </style>
