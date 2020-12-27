e697a76f14af6ff622a9c75a66f841b561a5aebc
Alexander Sels <alexandersels@hotmail.com>
2020-12-27 14:37:06 +0100

    .

--- src/components/Authentication/Logout.vue
+++ src/components/Authentication/Logout.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <PageTitle title="Logout"></PageTitle>
+    <div class="row">
+      <PrimaryButton text="Logout" :clicked=onLogoutClicked></PrimaryButton>
+    </div>
+  </div>
 </template>
 
 <script>
+import PrimaryButton from "@/components/Shared/PrimaryButton";
+import PageTitle from "@/components/Shared/PageTitle";
+import {state} from '@/components/Shared/state/variables'
+
 export default {
-  name: "Logout"
+  name: "Logout",
+  components: {PrimaryButton, PageTitle},
+  methods: {
+    onLogoutClicked() {
+      state.user = undefined;
+      state.token = undefined;
+      this.$router.push('/');
+    }
+  }
 }
 </script>
 
 <style scoped>
 .container {
   margin-top: 10rem;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
